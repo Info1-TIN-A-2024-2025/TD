333846130b21c313ea40273405708908c33047c0
pierrebressy <pierre.bressy@heig-vd.ch>
2024-09-26 08:51:12 +0000

    added Terminal.md

---
+++ TD20240920/td.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(int argc, const char *argv[])
+{
+	// calcul racine carrée de i en utilisant l'algorithme de Johns & Smith 
+	// cf my_algo_xyz.md
+	/*	
+	a = 2^i;
+	i <<= 3;
+	i *= j;
+	j |= 0x32;
+	*/
+	
+	puts("dobar dan mozete raditi ?"); 
+	puts("Hello !"); 
+	puts("Bonjour !"); 
+	puts("Guten tag !"); 
+	puts("Good morning !"); 
+	puts("Bom dia !"); 
+	puts("Buenos dias !"); 
+	puts("Buongiorno !");
+	puts("Dzien dobry !");
+	
+	return 0; //0...255 
+
+} 
