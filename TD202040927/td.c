830af82ea36c0f903aad8635e449e9a42299eaae
pierrebressy <pierre.bressy@heig-vd.ch>
2024-09-27 11:53:05 +0000

    added TD20240927

---
+++ TD202040927/td.c
@@ -0,0 +1,12 @@
+int main(int argc, const char *argv[])
+{
+	double a=0.; // create a variable named a initilized to 0. as a real value
+	double b=0.; // double precision (64bits)
+	double c=0.;
+
+	a=10.;
+	b=20.;
+	c=0.5;
+
+	return 0;
+}
